6a9bda25180ee6d803ac09de7238098ccad1f25b
Christopher Settles <christophersettles@gmail.com>
2023-06-09 09:09:17 -0700

    First commit, change name

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'ChrisSettles',
+  author: 'Chris Settes',
+  email: 'christopherRSettles@gmailcom',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
